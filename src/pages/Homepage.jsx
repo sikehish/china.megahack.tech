b3a9feefb3f84d347a3cb332eb73211671621d73
labhk <labh.k2003@gmail.com>
2023-07-13 13:27:53 +0530

    homepage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Styles from './Homepage.module.css'
+
+function Homepage() {
+  return (
+    <>
+
+        {/* Hero-Section */}
+
+        <div className={Styles.div1}>
+            <h1>Mega Hacks 2023</h1>
+        </div>
+
+        {/* WhoWeAre-Section */}
+
+        <div className={Styles.div1}>
+            <h1>WhoWeAre-Section</h1>
+        </div>
+
+        {/* Events-Section */}
+
+        {/* Sponsors-Section */}
+
+        {/* ContactUs-Section */}
+        
+
+    </>
+  )
+}
+
+export default Homepage
