a88c8234e05cb74dde2f7a3e9302be6d3c961c6c
Syed Hisham Akmal <hisham0502@gmail.com>
2023-07-19 20:35:03 +0530

    Updated i18n.json to have 4 events and updated PastEvents component

--- src/components/PastEvents.jsx
+++ src/components/PastEvents.jsx
@@ -1,8 +1,97 @@
+import { useEffect } from 'react';
+import './PastEvents.css';
+import Styles from './PastEvents.module.css'
+import { useTranslation, Trans } from 'react-i18next';
 
 function PastEvents() {
+
+    const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        const lng = navigator.language;
+        i18n.changeLanguage(lng)
+    }, [])
+
+  const lng = navigator.language
+
   return (
-    <div>PastEvents</div>
+    <>
+<div className={Styles.eventCards}>
+              <div className={Styles.eventCard}>
+                  <div className={Styles.eventImage}>
+                      <img src="/image-2@2x.png" alt="img" />
+                  </div>
+                  <div className={Styles.eventBar}>
+                      <div className={Styles.eleft}>{t('event.cardbar1')}</div>
+                      <div className={Styles.eright}>AI/ML</div>
+                  </div>
+                  <div className={Styles.cardContent2}>
+                      <div className={Styles.cardHead2}>{t('event.cardhead1')}</div>
+                      <div className={Styles.cardText2}>{t('event.cardtext1')}</div>
+                  </div>
+                  <div className={Styles.div3LearnMore2}>{t('event.learnmore')}</div>
+              </div>
+              <div className={Styles.eventCard}>
+                  <div className={Styles.eventImage}>
+                      <img src="/image-3@2x.png" alt="img" />
+                  </div>
+                  <div className={Styles.eventBar2}>
+                      <div className={Styles.eleft}>{t('event.cardbar2')}</div>
+                      <div className={Styles.eright}>Competitiveness</div>
+                  </div>
+                  <div className={Styles.cardContent2}>
+                      <div className={Styles.cardHead2}>{t('event.cardhead1')}</div>
+                      <div className={Styles.cardText2}>{t('event.cardtext1')}</div>
+                  </div>
+                  <div className={Styles.div3LearnMore2}>{t('event.learnmore')}</div>
+              </div>
+          </div>
+          <div className={Styles.eventMore}>
+              <div className={Styles.more}>{t('event.more')}</div>
+          </div>
+          </>
+
   )
 }
 
 export default PastEvents
+
+// function PastEvents() {
+//   return (
+//     <div className='events-cont'>
+//     <div className='past-events-heading'>
+//     <h3>EVENTS</h3>
+//     <h4>Wonderful past</h4>
+//     </div>
+//     <div className="grid-container">
+//     <div className="grid-item">
+//       <img src="image1.jpg" alt="Image 1" />
+//       <h3>Card 1 Title</h3>
+//       <p>Card 1 Content</p>
+//       <a href="#learn-more">Learn More</a>
+//     </div>
+//     <div className="grid-item">
+//       <img src="image1.jpg" alt="Image 1" />
+//       <h3>Card 2 Title</h3>
+//       <p>Card 2 Content</p>
+//       <a href="#learn-more">Learn More</a>
+//     </div>
+//     <div className="grid-item">
+//       <img src="image1.jpg" alt="Image 1" />
+//       <h3>Card 3 Title</h3>
+//       <p>Card 3 Content</p>
+//       <a href="#learn-more">Learn More</a>
+//     </div>
+//     <div className="grid-item">
+//       <img src="image1.jpg" alt="Image 1" />
+//       <h3>Card 4 Title</h3>
+//       <p>Card 4 Content</p>
+//       <a href="#learn-more">Learn More</a>
+//     </div>
+   
+//   </div>
+//   </div>
+//   )
+// }
+
+// export default PastEvents
